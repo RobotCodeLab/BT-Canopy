022dab05573bb6fac89bbb66db277a6e26f5896d
hodgespodge <samuelhodges3000@gmail.com>
2022-03-07 15:39:55 -0500

    changed package name

--- src/BTWatcher/src/monitor/src/tree_monitor.cpp
+++
@@ -1,6 +0,0 @@
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
-
-// Subscribe to 
-class TreeMonitor : public rclcpp::Node
-{
---
+++ src/bt_monitor/src/tree_listener.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include "rclcpp/rclcpp.hpp"
+
+#include "bt_monitor/msg/behavior_tree_log.hpp"
+
+class TreeListener : public rclcpp::Node
+{
+  public:
+    TreeListener() : Node("tree_listener")
+    {
+      // call callback function when a message is received
+      subscription_ = this->create_subscription<bt_monitor::msg::BehaviorTreeLog>(
+        "behavior_tree_log", 10, std::bind(&TreeListener::callback, this, std::placeholders::_1));
+    }
+
+  private:
+    void callback(const bt_monitor::msg::BehaviorTreeLog::SharedPtr msg)
+    {
+      std::cout << "Received message: " << msg->data << std::endl;
+    }
+
+    rclcpp::Subscription<bt_monitor::msg::BehaviorTreeLog>::SharedPtr subscription_;
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<TreeListener>());
+  rclcpp::shutdown();
+  return 0;
+
+  printf("hello world bt_monitor package\n");
+  return 0;
+}
