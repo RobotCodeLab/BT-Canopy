a32f20cccbc346a6bcc8d83cc98696fbaf27bbf5
sshodge2 <sshodge2@ncsu.edu>
2022-03-21 12:24:40 -0400

    added script for opening shell tabs and execing in

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # BTWatcher
+
+- `docker build -t btwatcher .`
+
+- `rocker --x11 --nvidia --name btwatcher_container btwatcher`
+
+- `rocker --x11 --devices /dev/dri/card0 -- --name btwatcher_container btwatcher`
+
+- `. multi-exec.sh {1-9}` 
+
+- `docker ps`
+  
+- `docker exec -it <container id> /bin/bash`
+
+- `docker system prune`
---
+++ multi-exec.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+container_name="btwatcher-container"
+
+# set num_tabs to the user's choice (default is 1)
+num_tabs=${1:-1}
+
+# if num_tabs is not a number or is less than 0, or greater than 9, exit
+if ! [[ $num_tabs =~ ^[0-9]+$ ]] || [ $num_tabs -lt 1 ] || [ $num_tabs -gt 9 ]; 
+then
+    echo "Invalid number of tabs. Please enter a number between 1 and 9."
+else
+
+    for ((i=1; i<=$num_tabs; i++))
+    do
+        gnome-terminal --tab -- docker exec -it $container_name /bin/bash
+    done
+
+fi
+
+
