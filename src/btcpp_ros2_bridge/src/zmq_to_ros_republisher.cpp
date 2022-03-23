7bf97f17974e7902c2fc644bea97859664780aaf
sshodge2 <sshodge2@ncsu.edu>
2022-03-23 12:03:27 -0400

    added ZMQ to ament_target_dependencies

--- src/btcpp_ros2_bridge/src/zmq_to_ros_republisher.cpp
+++ src/btcpp_ros2_bridge/src/zmq_to_ros_republisher.cpp
@@ -2,41 +2,58 @@
 #include "rclcpp/rclcpp.hpp"
 #include "tree_msgs/msg/behavior_tree_log.hpp"
 
+#include <chrono>
+
 #include <zmq.hpp>
 
-class TreeListener : public rclcpp::Node
+class zmq_to_ros_republisher : public rclcpp::Node
 {
   public:
-    TreeListener() : Node("zmq_to_ros_republisher")
+
+
+    
+    zmq_to_ros_republisher() : Node("zmq_to_ros_republisher")
     {
-      // call callback function when a message is received
-      subscription_ = this->create_subscription<tree_msgs::msg::BehaviorTreeLog>(
-        "behavior_tree_log", 10, std::bind(&TreeListener::callback, this, std::placeholders::_1));
+      publisher_ = this->create_publisher<tree_msgs::msg::BehaviorTreeLog>("/behavior_tree_log", 10);
+      timer_ = this->create_wall_timer(
+        std::chrono::milliseconds(100),
+        std::bind(&zmq_to_ros_republisher::timer_callback, this));
+
     }
+
 
   private:
-    void callback(const tree_msgs::msg::BehaviorTreeLog::SharedPtr msg)
+
+    void timer_callback()
     {
-      // std::cout << "Received message: " << msg-> << std::endl;
+      zmq::context_t context(1);
+      // zmq::socket_t socket(context, ZMQ_SUB);
+      // socket.connect("tcp://localhost:5555");
+      // socket.setsockopt(ZMQ_SUBSCRIBE, "", 0);
 
-      auto events = msg.get()->event_log;
 
-      // RCLCPP_INFO(this->get_logger(), "Received message: %s", msg->event_log);
+      //print current time
+      auto now = std::chrono::system_clock::now();
+      auto now_ms = std::chrono::time_point_cast<std::chrono::milliseconds>(now);
+      auto value = now_ms.time_since_epoch();
+      auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(value);
+      std::cout << "Current time: " << millis.count() << std::endl;
 
-      for (auto event : events)
-      {
-        std::cout << event.node_name << " " << event.current_status << std::endl;
-      }
-
+      // tree_msgs::msg::BehaviorTreeLog msg;
+      // msg.timestamp = std::chrono::system_clock::now();
+      // msg.event_log.push_back(tree_msgs::msg::BehaviorTreeStatusChange());
     }
+    
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<tree_msgs::msg::BehaviorTreeLog>::SharedPtr publisher_;
 
-    rclcpp::Subscription<tree_msgs::msg::BehaviorTreeLog>::SharedPtr subscription_;
+    
 };
 
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<TreeListener>());
+  rclcpp::spin(std::make_shared<zmq_to_ros_republisher>());
   rclcpp::shutdown();
   return 0;
 
