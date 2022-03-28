b9a2f6b54b03944a0ff91980ff4924b29d1fd314
hodgespodge <samuelhodges3000@gmail.com>
2022-03-27 23:20:51 -0400

    added script for launching turtlebot easily

---
+++ dev-tools/turtlebot_startup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source install/setup.bash
+
+export TURTLEBOT3_MODEL=burger
+
+gnome-terminal --tab -- ./build/groot/Groot
+
+sleep 2
+
+gnome-terminal --tab -- ros2 launch turtlebot3_gazebo turtlebot3_world.launch.py
+
+sleep 10
+
+gnome-terminal --tab -- ros2 launch slam_toolbox online_async_launch.py use_sim_time:=True
+
+sleep 5
+
+gnome-terminal --tab -- ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True
+
+sleep 5
