bf6884f5554611c3dc5fccd647a51d763a6d2617
hodgespodge <samuelhodges3000@gmail.com>
2022-05-17 11:25:44 -0400

    temporarily combined publisher and server nodes

--- src/behaviortree_cpp_v3_ros2_publisher/include/behaviortree_cpp_v3_ros2_publisher/bt_ros_publisher.hpp
+++ src/behaviortree_cpp_v3_ros2_publisher/include/behaviortree_cpp_v3_ros2_publisher/bt_ros_publisher.hpp
@@ -7,6 +7,8 @@
 #include "tree_msgs/msg/status_change.hpp"
 #include "tree_msgs/msg/status_change_log.hpp"
 
+#include "tree_msgs/srv/get_tree_nodes.hpp"
+
 class BTRosPublisher : public BT::StatusChangeLogger
 {
     public:
@@ -17,9 +19,98 @@ class BTRosPublisher : public BT::StatusChangeLogger
             clock_ = node->get_clock();
             // logger_ = node->get_logger();
 
+            //
+            root_node_ = tree.rootNode();
+            tree_nodes_ = tree.nodes;
+            //
+
             tree_name = tree.rootNode()->name();
             status_change_log_pub_ = node->create_publisher<tree_msgs::msg::StatusChangeLog>(
                 "/status_change_log", 10);
+
+
+            //
+            auto handle_add = [this](const std::shared_ptr<rmw_request_id_t> request_header,
+                                const std::shared_ptr<tree_msgs::srv::GetTreeNodes::Request> request,
+                                const std::shared_ptr<tree_msgs::srv::GetTreeNodes::Response> response) -> void {
+
+            // Set request to void otherwise colcon throws an error
+            (void) request_header;
+            (void) request;
+
+            response->behavior_tree.root_uid = root_node_->UID();
+            response->behavior_tree.tree_name = root_node_->name();
+
+            for (auto tree_node_ptr : tree_nodes_) {
+                BT::TreeNode * tree_node = tree_node_ptr.get();
+                tree_msgs::msg::TreeNode node_msg;
+                
+                node_msg.uid = tree_node->UID();
+                
+                if (auto control = dynamic_cast<const BT::ControlNode*>(tree_node))
+                {
+                    for (const auto& child : control->children())
+                    {
+                        node_msg.children_uid.push_back(child->UID());
+                    }
+                } else if (auto decorator = dynamic_cast<const BT::DecoratorNode*>(tree_node))
+                {
+                    node_msg.children_uid.push_back(decorator->child()->UID());
+                }
+
+                switch(tree_node->type()){
+                    case BT::NodeType::UNDEFINED:
+                        node_msg.type = node_msg.UNDEFINED;
+                        break;
+                    case BT::NodeType::ACTION:
+                        node_msg.type = node_msg.ACTION;
+                        break;
+                    case BT::NodeType::CONDITION:
+                        node_msg.type = node_msg.CONDITION;
+                        break;
+                    case BT::NodeType::CONTROL:
+                        node_msg.type = node_msg.CONTROL;
+                        break;
+                    case BT::NodeType::DECORATOR:
+                        node_msg.type = node_msg.DECORATOR;
+                        break;
+                    case BT::NodeType::SUBTREE:
+                        node_msg.type = node_msg.SUBTREE;
+                        break;
+                    default:
+                        break;
+                }
+
+                switch(tree_node->status()){
+                    case BT::NodeStatus::IDLE:
+                        node_msg.status.value = node_msg.status.IDLE;
+                        break;
+                    case BT::NodeStatus::RUNNING:
+                        node_msg.status.value = node_msg.status.RUNNING;
+                        break;
+                    case BT::NodeStatus::SUCCESS:
+                        node_msg.status.value = node_msg.status.SUCCESS;
+                        break;
+                    case BT::NodeStatus::FAILURE:
+                        node_msg.status.value = node_msg.status.FAILURE;
+                        break;
+                    default:
+                        break;
+                }
+
+                node_msg.instance_name = tree_node->name();
+                node_msg.registration_name = tree_node->registrationName();
+
+
+            }
+
+            response->success = true;
+        };
+
+        service_ = node->create_service<tree_msgs::srv::GetTreeNodes>("get_tree_nodes", handle_add);
+
+        //
+        
         }
 
         void callback(
@@ -58,5 +149,9 @@ class BTRosPublisher : public BT::StatusChangeLogger
         std::vector<tree_msgs::msg::StatusChange> state_changes;
 
         std::string tree_name;
+
+        rclcpp::Service<tree_msgs::srv::GetTreeNodes>::SharedPtr service_;
+        std::vector<BT::TreeNode::Ptr> tree_nodes_;
+        BT::TreeNode * root_node_;
     
 };
