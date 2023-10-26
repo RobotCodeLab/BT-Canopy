ba7b2a2e3758a87568d89824a62bf8cc5e97de09
John-Paul Ore <579990+jpwco@users.noreply.github.com>
2023-10-25 23:46:33 -0400

    modified plots

--- examples/nav2_with_canopy/case_study/plot_generator.py
+++ examples/nav2_with_canopy/case_study/plot_generator.py
@@ -2,12 +2,14 @@ import plotly.express as px
 import pandas as pd
 
 navigate_through_poses = pd.read_csv("./navigate_through_poses_coverage.csv")
+df = navigate_through_poses
+df['Percent Covered'] = df['Percent Covered'].apply(lambda x: x*100)
 fig = px.scatter(navigate_through_poses, x="Percent Covered", y="Coverage Type", color_discrete_sequence=["#440154", "#21918c"], orientation="h")
-fig.update_traces(marker_size=50)
+fig.update_traces(marker_size=25)
 fig.update_layout(
     plot_bgcolor="white",
     font=dict(
-        size=30,
+        size=40,
     ),
 )
 fig.update_xaxes(
@@ -16,7 +18,8 @@ fig.update_xaxes(
     showline=True,
     linecolor="black",
     gridcolor="lightgrey",
-    range=[0, 1],
+    range=[0, 100],
+    dtick=25
 )
 fig.update_yaxes(
     mirror=True,
@@ -25,14 +28,17 @@ fig.update_yaxes(
     linecolor="black",
     gridcolor="lightgrey",
 )
+fig.update_layout(height=350, xaxis_ticksuffix = '%')
 fig.show()
 
 navigate_to_pose = pd.read_csv("./navigate_to_pose_coverage.csv")
+df = navigate_to_pose
+df['Percent Covered'] = df['Percent Covered'].apply(lambda x: x*100)
 fig = px.box(navigate_to_pose, x="Percent Covered", y="Coverage Type", color_discrete_sequence=["#440154", "#21918c"])
 fig.update_layout(
     plot_bgcolor="white",
     font=dict(
-        size=30,
+        size=40,
     ),
 )
 fig.update_xaxes(
@@ -41,7 +47,8 @@ fig.update_xaxes(
     showline=True,
     linecolor="black",
     gridcolor="lightgrey",
-    range=[0, 1],
+    range=[0, 100],
+    dtick=25
 )
 fig.update_yaxes(
     mirror=True,
@@ -50,19 +57,22 @@ fig.update_yaxes(
     linecolor="black",
     gridcolor="lightgrey",
 )
+fig.update_layout(height=350, xaxis_ticksuffix = '%')
 fig.show()
 
 lcov = pd.read_csv("./lcov_coverage.csv")
+df = lcov
+df['Percent Covered'] = df['Percent Covered'].apply(lambda x: x*100)
 fig = px.scatter(lcov, x="Percent Covered", y="Coverage Type", color="Nav2 Package", color_discrete_sequence=["#440154", "#21918c"])
-fig.update_traces(marker_size=50)
+fig.update_traces(marker_size=25)
 fig.update_layout(
     plot_bgcolor="white",
-    legend=dict(
-        x=0,
-        y=0,
-    ),
+    # legend=dict(
+    #     x=0,
+    #     y=0,
+    # ),
     font=dict(
-        size=30,
+        size=40,
     ),
 )
 fig.update_xaxes(
@@ -71,7 +81,8 @@ fig.update_xaxes(
     showline=True,
     linecolor="black",
     gridcolor="lightgrey",
-    range=[0, 1],
+    range=[0, 100],
+    dtick=25
 )
 fig.update_yaxes(
     mirror=True,
@@ -80,5 +91,6 @@ fig.update_yaxes(
     linecolor="black",
     gridcolor="lightgrey",
 )
+fig.update_layout(height=350, xaxis_ticksuffix = '%')
 fig.show()
 
